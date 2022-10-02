b28e43d395fe9fca0acbd2f7359cc7bb86342d1f
Abdul Rehman Kalsekar <78557959+arkalsekar@users.noreply.github.com>
2022-10-02 19:23:05 +0530

    Added Key Press Tracker Scrilpt

---
+++ Key_Press_Tracker/main.py
@@ -0,0 +1,21 @@
+import keyboard
+import time
+from selenium import webdriver
+import requests
+
+def url(geturl):
+    rl = requests.get(geturl)
+    print("Waiting " + str(geturl)
+
+# keyboard.write("Hello World here is me Abd Virus")
+
+recorded = keyboard.record(until='esc')
+keyboard.start_recording()
+# time.sleep(5)
+a = keyboard.play(recorded, speed_factor=3.0)
+
+# 5201j6wn3d Abdul Rehman Kalsekar# j6wn3d Abdul Rehman Kalsekar
+
+# How to Say that and What are you going To Do Here... 
+
+# How to Stay Motivated while Learning Data Science, AI and Machine Learning.. 
